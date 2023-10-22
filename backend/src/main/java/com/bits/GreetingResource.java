ba9d109bf24c5a3b058c5cd93b74923e58a32a70
TheComputerM <mudit.somani00@gmail.com>
2023-10-23 02:44:23 +0530

    refactor: remove unused endpoint

--- backend/src/main/java/com/bits/GreetingResource.java
+++ backend/src/main/java/com/bits/GreetingResource.java
@@ -1,8 +1,6 @@
 package com.bits;
 
-import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -14,11 +12,5 @@ public class GreetingResource {
   @Produces(MediaType.TEXT_PLAIN)
   public String hello() {
     return "Hello from RESTEasy Reactive";
-  }
-
-  @POST
-  @Transactional
-  public String test() {
-    return "Test";
   }
 }
