5162555c5d369f69838adebc47f77bf68a283ae4
TheComputerM <mudit.somani00@gmail.com>
2023-10-28 12:20:07 +0530

    feat: add product entity

---
+++ backend/src/main/java/com/bits/ProductResource.java
@@ -0,0 +1,27 @@
+package com.bits;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.bits.entities.Product;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+
+@Path("/product")
+public class ProductResource {
+
+  @GET
+  public List<Product> listAll() {
+    return Product.listAll();
+  }
+
+  @POST
+  @Transactional
+  public UUID create(Product product) {
+    product.persist();
+    return product.id;
+  }
+}
