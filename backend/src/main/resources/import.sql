757778c845324da7863111a0f2a2f5816f4baaa4
TheComputerM <mudit.somani00@gmail.com>
2023-11-09 01:33:41 +0530

    feat: add demo users

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -4,3 +4,9 @@
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
+
+INSERT INTO "users" ("balance", "id", "avatar", "name") VALUES
+(100, '874f37f8-0fe4-4b4f-ab56-af67d61a67ef',	'https://i.pravatar.cc/300?img=1', 'Mudit Somani'),
+(200, 'cc0828fe-af80-498b-b4a0-cbb1a162debd',	'https://i.pravatar.cc/300?img=2', 'Varun Reddy'),
+(300, '18062a20-5738-4126-90f5-4e48fdfa5afd',	'https://i.pravatar.cc/300?img=3', 'Eshaan Sudan'),
+(400, '0330bb23-2566-4456-b8a7-f11bd2b45462',	'https://i.pravatar.cc/300?img=4', 'Praharsh Vitta');
