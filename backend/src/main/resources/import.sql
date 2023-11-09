ade76ddd5d470c762d754e8849dbd7c825877020
eshaan416 <sudaneshaan@gmail.com>
2023-11-09 23:03:43 +0530

    feat : add demo chatrooms

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -10,3 +10,8 @@ INSERT INTO "users" ("balance", "id", "avatar", "name") VALUES
 (200, 'cc0828fe-af80-498b-b4a0-cbb1a162debd',	'https://i.pravatar.cc/300?img=2', 'Varun Reddy'),
 (300, '18062a20-5738-4126-90f5-4e48fdfa5afd',	'https://i.pravatar.cc/300?img=3', 'Eshaan Sudan'),
 (400, '0330bb23-2566-4456-b8a7-f11bd2b45462',	'https://i.pravatar.cc/300?img=4', 'Praharsh Vitta');
+
+INSERT INTO "chatroom" ("buyer_last_read", "seller_last_read", "buyer_id", "id", "seller_id") VALUES
+('2023-10-09 17:18:57.491209+00',	'2023-11-09 17:18:57.491209+00',	'874f37f8-0fe4-4b4f-ab56-af67d61a67ef',	'68d7183a-bfa2-4350-90cb-6788051a72d8',	'0330bb23-2566-4456-b8a7-f11bd2b45462'),
+('2023-11-08 17:19:40.440876+00',	'2023-11-09 17:19:40.440876+00',	'cc0828fe-af80-498b-b4a0-cbb1a162debd',	'f436e0f6-4779-45a1-a6fd-ed5ee34e4d9b',	'18062a20-5738-4126-90f5-4e48fdfa5afd'),
+('2023-11-5 7:20:13.157245+00',	'2023-11-09 10:20:13.157245+00',	'0330bb23-2566-4456-b8a7-f11bd2b45462',	'99b637d2-ec1f-4d46-9514-fb79c2d9762b',	'cc0828fe-af80-498b-b4a0-cbb1a162debd');
