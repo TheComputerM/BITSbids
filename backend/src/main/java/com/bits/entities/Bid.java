37ef4bf849d4412ed24014bec5022653d66de376
TheComputerM <mudit.somani00@gmail.com>
2023-11-19 13:46:04 +0530

    fix: cascade on delete

--- backend/src/main/java/com/bits/entities/Bid.java
+++ backend/src/main/java/com/bits/entities/Bid.java
@@ -4,16 +4,15 @@ import java.time.Instant;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -25,12 +24,12 @@ public class Bid extends PanacheEntityBase {
     @GeneratedValue
     public UUID id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id")
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
     public Product product;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "bidder_id")
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
     public User bidder;
 
     public Integer amount;
--- backend/src/main/java/com/bits/entities/Message.java
+++ backend/src/main/java/com/bits/entities/Message.java
@@ -4,13 +4,13 @@ import java.util.UUID;
 import java.time.Instant;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -21,8 +21,8 @@ public class Message extends PanacheEntityBase {
   @GeneratedValue
   public UUID id;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "room_id")
+  @ManyToOne
+  @OnDelete(action = OnDeleteAction.CASCADE)
   public Chatroom room;
 
   @Column(columnDefinition = "TEXT")
